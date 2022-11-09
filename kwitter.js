b463270b18d6c840b80b2208e59e64dc64eddfd5
KK13KANISHK <108186231+KK13KANISHK@users.noreply.github.com>
2022-11-09 20:19:52 +0530

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,13 @@
+    function addUser() 
+    {
+        user_name = document.getElementById("user_name").value;
+        localStorage.setItem("user name", user_name);
+
+        password = document.getElementById("password").value;
+        localStorage.setItem("password", password);
+
+        window.location = "kwitter_room.html";
+
+        
+    }
+
